7b2b787d8188ae88d74c994572059d21461fab3d
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2006-10-07 12:06:01 +0000

    git-svn-id: file:///home/jotik/develop/bibletime/sf_backup/svn-13.12.2011@35 ba6c7691-6512-0410-bf18-b6da21b6447f

--- trunk/bibletime2/data/versification/do_import_db.sh
+++ trunk/bibletime2/data/versification/do_import_db.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "\nTrying to compile and run the import tool\n";
+echo -e "\nUnpacking the source files\n"
+(cd ccel; gunzip *gz);
+
+echo -e "\nTrying to compile and run the import tool\n";
 qmake import_db.pro && make && ./import_db;
 
-echo "\nThe schema and mapping databases are in db/, if everything went ok.\n";
-echo "Cleaning up."
+echo -e "\nThe schema and mapping databases are in db/, if everything went ok.\n";
+echo -e "Cleaning up."
 make distclean;
