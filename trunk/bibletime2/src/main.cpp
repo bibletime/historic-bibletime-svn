33a9dafb487875e8989303e3048bb9da18042d0c
mgruner <mgruner@ba6c7691-6512-0410-bf18-b6da21b6447f>
2006-10-23 19:59:43 +0000

    added project stubs
    git-svn-id: file:///home/jotik/develop/bibletime/sf_backup/svn-13.12.2011@40 ba6c7691-6512-0410-bf18-b6da21b6447f

---
+++ trunk/bibletime2/src/main.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Martin Gruner   *
+ *   mg.pub@gmx.net   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+//QT includes
+#include <QString>
+#include <QtDebug>
+
+int main(int argc, char *argv[])
+{
+	qDebug() << "Hi. This is BibleTime2. Almost.";
+	return EXIT_SUCCESS;
+}
