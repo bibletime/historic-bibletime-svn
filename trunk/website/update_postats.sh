1230fefb5a3a503139e92e2ca14c7b76be8fa675
joachim <joachim@ba6c7691-6512-0410-bf18-b6da21b6447f>
2008-01-29 20:02:43 +0000

    Changed website translation statistics to include Qt .ts files
    git-svn-id: file:///home/jotik/develop/bibletime/sf_backup/svn-13.12.2011@613 ba6c7691-6512-0410-bf18-b6da21b6447f

--- trunk/website/update_postats.sh
+++ trunk/website/update_postats.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 (cd website-generated/postats && ./make_postats.pl)
+(cd website-generated/postats && ./update_qt_stats.sh)
--- trunk/website/website-common/postats/make_postats.pl
+++ trunk/website/website-common/postats/make_postats.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # This script processes the makefiles and creates the file which contains the necessary
 # entries so the SSI script can create the translation statistics
 
@@ -7,7 +7,7 @@ use lib "../Perl";
 use Locale::PO;
 use strict;
 
-my $BT_HOME="/home/jansorg/Projekte/BibleTime";
+my $BT_HOME="/home/jansorg/Projekte/C++/BibleTime";
 my $WEB_HOME="$BT_HOME/bibletime-website";
 
 sub get_langs {
@@ -21,7 +21,7 @@ sub get_langs {
 	return @langs;
 }
 
-sub parse_pofile(){
+sub parse_pofile {
 	my $pofile = shift;
 	my $lang   = shift;
 
@@ -57,8 +57,27 @@ sub parse_pofile(){
 	return "$lang \t$translated \t$untranslated \t$fuzzy\n";
 }
 
+sub parse_tsfile {
+	print "WOrking on ts file...\n";
+	my $file = shift;
+	my $lang = shift;
+
+	my ($translated, $untranslated) = (0,0);
+
+	open(IN, "< $file") || return "$lang \t0 \t0 \t0\n";
+
+	while (<IN>) {
+		$untranslated++ if ($_ =~ /<translation><\/translation>||<translation type="unfinished">/);	
+		$translated++ if ($_ =~ /<translation>.+<\/translation>$/);	
+	}
+
+	close(IN);
+
+	return "$lang \t$translated $untranslated \t0\n";
+}
+
 # Create statistics for the given language
-sub bibletime_stats() {
+sub bibletime_stats {
 	my $sourcedir = shift;
 	my $targetfile = shift;
 	my @files;
@@ -71,12 +90,36 @@ sub bibletime_stats() {
 	closedir(DIR);
 
 	open(FILE, "> $targetfile");
-	foreach my $pofile (sort(@files)) {
-		my $lang = $pofile;
+	foreach my $file (sort(@files)) {
+		my $lang = $file;
 		$lang =~ s/(\.po)$//;
-		print FILE &parse_pofile("$sourcedir/$pofile", "$lang");
+		print FILE &parse_pofile("$sourcedir/$file", "$lang");
 	}
 	close(FILE);
+}
+
+sub qt_stats {
+	my $sourcedir = shift;
+	my $targetfile = shift;
+	my @files;
+
+	print "$sourcedir\n";
+
+        opendir(DIR, $sourcedir) || die "Can't open dir $sourcedir";
+        while (my $pofile = readdir(DIR)) {
+                next unless ($pofile =~ /(\.ts)$/);
+                push (@files, $pofile);
+        }
+        closedir(DIR);
+
+        open(FILE, "> $targetfile") || die "Can't open $targetfile for writing";
+        foreach my $pofile (sort(@files)) {
+                my $lang = $pofile;
+                $lang =~ s/(\.ts)$//;
+                print FILE &parse_tsfile("$sourcedir/$pofile", "$lang");
+        }
+        close(FILE);
+	
 }
 
 #website stats
@@ -88,6 +131,8 @@ foreach my $lang (@langs){
 }
 close(OUT);
 
-&bibletime_stats( "$BT_HOME/bibletime-i18n/po", 	 "messages_stats.txt" );
-&bibletime_stats( "$BT_HOME/bibletime-i18n/po/howto", 	 "howto_stats.txt" );
-&bibletime_stats( "$BT_HOME/bibletime-i18n/po/handbook", "handbook_stats.txt" );
+&qt_stats( "$BT_HOME/bibletime-2.0/i18n/messages", "i18n.txt" );
+
+#&bibletime_stats( "$BT_HOME/bibletime-i18n/po", 	 "messages_stats.txt" );
+#&bibletime_stats( "$BT_HOME/bibletime-i18n/po/howto", 	 "howto_stats.txt" );
+#&bibletime_stats( "$BT_HOME/bibletime-i18n/po/handbook", "handbook_stats.txt" );
