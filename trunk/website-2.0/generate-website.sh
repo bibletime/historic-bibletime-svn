56a2db095d88d3b99efd193852799eca999abff6
tabthorpe <tabthorpe@ba6c7691-6512-0410-bf18-b6da21b6447f>
2009-04-21 03:50:41 +0000

    - Use coreutils gcp for FreeBSD, -u is very useful :-)
    git-svn-id: file:///home/jotik/develop/bibletime/sf_backup/svn-13.12.2011@1175 ba6c7691-6512-0410-bf18-b6da21b6447f

--- trunk/website-2.0/generate-website.sh
+++ trunk/website-2.0/generate-website.sh
@@ -87,9 +87,9 @@ function fixTranslation {
 
 #Copies the common website files into the website-generated dir
 function copyResources {
-	export _cpflags="-Rafu"
-	if test $OSTYPE = FreeBSD; then export _cpflags="-Raf"; fi
-	cp ${_cpflags} website-common/* website-generated/
+	export _cpcmd="cp"
+	if test $OSTYPE = FreeBSD; then export _cpcmd="gcp"; fi
+	${_cpcmd} -Rafu website-common/* website-generated/
 	find website-generated/ -iname \.svn | xargs rm -rf
 }
 
